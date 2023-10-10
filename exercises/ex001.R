44ce2966418777cbf2e3a017a8d5270b03019b38
rutysara <139557312+rutysara@users.noreply.github.com>
2023-10-10 15:44:48 +0100

    Update ex001.R

---
+++ exercises/ex001.R
@@ -0,0 +1,29 @@
+# This is a COMMENT
+
+# KEYBOARD SHORTCUTS
+# Clean Console Window (no deletion) = CTRL + L
+
+# Creating variables
+# To execute click over the line or a group of lines and press CTRL (or CMD) + Enter
+a <- 10
+a
+
+# You cannot execute variables if without giving the value first
+b
+b = 10
+b
+B 
+# Variables are case-sensitive
+
+# To create variable you can use <- or = to give it a value. However, <- works in all situations while = has exceptions
+
+# STANDARD 1 : <- for variables and = for functions
+
+# The orientation of the arrow (<- or ->) is irrelevant as long as the arrow is pointing to the variable that you want to attribute a value to
+c -> 10 #error, in any circumstance  you can attribute a value to a value
+10 -> c
+
+# As soon as a variable has a value, you can attribute the variable's value to another variable
+c -> d
+
+# STANDARD 2 : "Variable" <- "Value"

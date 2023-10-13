42e5e0cc1b4b44f0089b28a0bb4fbe15b56637f5
rutysara <139557312+rutysara@users.noreply.github.com>
2023-10-13 09:26:59 +0100

    Update ex001.R

--- exercises/ex001.R
+++ exercises/ex001.R
@@ -9,10 +9,10 @@ a <- 10
 a
 
 # You cannot execute variables if without giving the value first
-b
+b = NULL
 b = 10
 b
-B 
+B = NULL
 # Variables are case-sensitive
 
 # To create variable you can use <- or = to give it a value. However, <- works in all situations while = has exceptions
@@ -20,10 +20,61 @@ B
 # STANDARD 1 : <- for variables and = for functions
 
 # The orientation of the arrow (<- or ->) is irrelevant as long as the arrow is pointing to the variable that you want to attribute a value to
-c -> 10 #error, in any circumstance  you can attribute a value to a value
+# c -> 10 = error, in any circumstance  you can attribute a value to a value
 10 -> c
 
 # As soon as a variable has a value, you can attribute the variable's value to another variable
 c -> d
 
-# STANDARD 2 : "Variable" <- "Value"
+# STANDARD 2 : "Variable" <- Numerical Value or "String"
+# Note that Strings can be Numerical too
+a <- 10
+b <- 5
+c <- a + b
+
+# You can re-create variable that you already used and it will override the previous value
+a
+a <- "Rutysara"
+b <- "Matheus"
+
+# If you want to have both character values in one variable you have to use functions
+
+# Functions
+# They are part of packages and need to be installed
+# Non-Standard packages have to be called?
+
+c <- c(a,b)
+# Here, variable C was created using a FUNCTION that has letter c but it is not a variable and the system know is because of ()
+c
+
+# Variable C now has length (2) because it contains 2 variables
+# This is not related to the number of characters or words
+
+# HELP : Add ? before a Function
+?c
+
+d <- c(10,5,15,20)
+d
+
+# Using [] will allow to call the value or string inside a function
+# The number inside of [] corresponds to the position inside the function
+d[1]
+d[2]
+d[3]
+d[4]
+d[5] # NA
+
+# Summary Function
+?summary
+
+
+
+
+
+
+
+
+
+
+
+

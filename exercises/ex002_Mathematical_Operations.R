3d4d759884918bd3a86a45b1883c52f3031ca69a
rutysara <139557312+rutysara@users.noreply.github.com>
2023-10-26 17:06:48 +0100

    Finish ex002 and start ex003

---
+++ exercises/ex002_Mathematical_Operations.R
@@ -0,0 +1,59 @@
+# Mathematical Operations
+
+# Basic Methods
+
+# Sum
+5 + 5
+
+# Subtraction
+10 - 8
+
+# Division
+9 / 3
+
+# Multiplication
+4 * 2
+
+# Power
+4 ** 2
+4 ^ 2
+
+# Besides calculations, you can also ask questions to know quickly True or False questions
+# Equal?
+5 == 6
+5 == 5
+
+# Different?
+5 != 6
+5 != 5
+
+# Both Greater or Lesser than?
+4 > 2
+2 > 4
+
+3 < 2
+3 < 4
+
+# Both Greater (or Lesser) or Equal?
+4 >= 5
+4 >= 4
+
+6 <= 8
+6 <= 5
+
+# AND
+6 == 6 & 7 == 8
+# Both have to be True
+6 == 6 & 7 != 8
+
+# OR
+6 == 6 | 7 == 8
+# In this case, if only one is True, the final reply will be True because OR is used
+
+6 != 6 | 7 == 8
+5 > 3 | 5 < 8
+
+# NEGATE or CONTRARY
+!6 == 6
+!4 > 2
+
---
+++ exercises/ex003_Numeric_Data.R
@@ -0,0 +1,3 @@
+# OBJECTIVE : Learn how R organises data
+
+# 
